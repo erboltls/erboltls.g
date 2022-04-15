5e345e3a9d11c76f46889ef2a660dfbe461a99c2
erboltls <103302289+erboltls@users.noreply.github.com>
2022-04-15 03:22:11 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// var typed = new Typed(".headerTitle", {
+// String: [
+//     "Designer",
+//     "Developer",
+//     "Freelancer"
+// ],
+//     typeSpeed: 60,
+//     backSpeed: 60,
+//     loop: true
+// });
+
+
+
+//         ('document').ready(function(){
+//     var typed = new Typed('#typed',{
+//         strings:['First sentence', 'Second sentence'],
+//         backSpeed: 40,
+//         typeSpeed: 40
+     
+//     });
+// });
